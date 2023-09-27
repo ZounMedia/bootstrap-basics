735334e175cf5c86690344a8b46e79e31348ee1e
Romaric Zounlome <rzounlom@gmail.com>
2023-09-27 06:20:06 -0400

    complete basic landing page example

---
+++ bootstrap-basics/countdown-clock/script.js
@@ -0,0 +1,26 @@
+// Function to update the countdown clock
+function updateCountdown() {
+  const countdownDate = new Date(
+    document.getElementById("countdownDate").value
+  ).getTime();
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+}
+
+// Event listener for the "Start Countdown" button
+document.getElementById("startButton").addEventListener("click", function () {
+  updateCountdown();
+  setInterval(updateCountdown, 1000);
+});
